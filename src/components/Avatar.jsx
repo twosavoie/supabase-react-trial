3d36c01d49f582022f31d11167f2fcebf46bdf7a
Lisa Savoie <twosavoie@gmail.com>
2026-06-26 16:12:39 -0500

    Moved Avatar component to components

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient";
+import { supabase } from "../supabaseClient";
 import PropTypes from "prop-types";
 
 export default function Avatar({ url, size, onUpload }) {
